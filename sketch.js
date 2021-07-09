c2e5573e4d45278d61dd6bc3f95688d4366fac07
vansh1425 <73531118+vansh1425@users.noreply.github.com>
2021-07-09 20:04:02 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 var ironman,img1,img2,img3,img4,img5,img6,img7;
+var I1,I2,gameoverImg,gameover;
 var backgroundImg,b2,planet;
 var laser,laser1,laser2,lai1,lai2,lai3,laserGroup,laserGroup1,laserGroup2;
 var thanoes,t1,t2,t3,t4;
@@ -6,7 +7,8 @@ var obstaclesGroup,asteroid,spaceship,power;
 var repulsorsound;
 var planetGroup,Moon,alienPlanet,BlackHole;
 var ground;
- var life = 100 ;
+
+var Score = 0;
 var gameState = "serve";
 function preload(){
 img1 =loadImage("ironman.png");
@@ -28,9 +30,13 @@ power = loadImage("power.png");
 img7 = loadImage("ironman 1.png");
 lai3 = loadImage("lase3.png");
 repulsorsound = loadSound("repulsor sound.mp3");
+
 alienPlanet = loadImage("planet.png");
 Moon = loadImage("moon.png");
-BlackHole = loadImage("blackhole.png")
+BlackHole = loadImage("blackhole.png");
+I1 = loadImage("ironman 3 .png");
+I2 = loadImage("ironman 4.png");
+gameoverImg = loadImage("download.png");
 }
 function setup() {
   createCanvas(800,500);
@@ -38,12 +44,17 @@ function setup() {
   ground.addImage(backgroundImg);
   ground.scale=5.4;
   planetGroup =new Group();
-  ironman =createSprite(150, 370, 50, 50);
- 
+  ironman =createSprite(150, 370);
+  ironman.setCollider("circle",0,0,60);
+  ironman.debug= false;
  thanoes =createSprite(650,380,50,50);
  thanoes.addImage(t1);
  
- 
+ gameover = createSprite(200,200);
+ gameover.addImage(gameoverImg);
+ gameover.visible=false;
+
+ life = 100 ;
  laserGroup = new Group();
  laserGroup1 = new Group();
  laserGroup2 =new Group();
@@ -55,6 +66,7 @@ function draw() {
  
   if (gameState === "serve") {
     serve();
+    
      }
    if(keyDown("U")){
      gameState="play";
@@ -64,10 +76,11 @@ function draw() {
   if(gameState === "play"){
     spawnPlanets(); 
     ironman.addImage(img1);
+
     if (ground.x < 0){
       ground.x = ground.width/2;
     }
-    ground.velocityX=-2;
+    ground.velocityX=-(1+Score/100);
    
     if(keyDown (UP_ARROW) ){
     ironman.y = ironman.y-5;
@@ -82,40 +95,71 @@ function draw() {
   }
 
    if(keyDown(RIGHT_ARROW)){
-    //ironman.velocityX=2;
     ironman.x=ironman.x+5;
     ironman.addImage(img2);
-    //ironman.scale=0.1;
+ 
      }
      if(keyDown(LEFT_ARROW)){
     
      ironman.x=ironman.x-5; 
      ironman.addImage(img2);
-      //ironman.scale=0.1;
+      
        }
+       
+ 
+      if(ironman.isTouching(obstaclesGroup)){
+        life=life-1;
+        obstaclesGroup.destroyEach();
+        ironman.addImage(I1);
+      }
       
        if(keyDown("space")){
          ironman.addImage(img4);
          repulsorsound.play();
        createlaser();
-        
+       
         }
        
         if(keyDown("Z")){
           repulsorsound.play();
           ironman.addImage(img5);
           createlaser1();
-         
+          
         }
         if(keyDown("X")){
           ironman.addImage(img7);
-       
-          createlaser2()
+        
+          createlaser2();
+          
         }
+        if(laserGroup.isTouching(obstaclesGroup)){
+          obstaclesGroup.destroyEach();
+          Score = Score+3;
+        }
+        if(laserGroup1.isTouching(obstaclesGroup)){
+          obstaclesGroup.destroyEach();
+          Score = Score+1;
+        }
+        if(laserGroup2.isTouching(obstaclesGroup)){
+          obstaclesGroup.destroyEach();
+          Score = Score+2;
+        }
+        if(life === 0){
+          gameState = "end";
+        }
+        if(gameState=== "end"){
+          ironman.addImage(I2);
+          endState();
+        }
+        
         spawnObstacles();   
         
       }
   drawSprites();
+  textSize(30);
+  fill("red");
+  text("Life ="+life,50,60);
+  text("Score ="+Score,300,50);
 }
 function createlaser() {
   laser= createSprite(100, 100, 60, 10);
@@ -154,17 +198,27 @@ function createlaser2() {
    
 }
 function serve(){
- 
+  
+ Score.visible = false;
+ life.visible = false;
 ironman.addImage(img6);
 thanoes.addImage(t3);
+}
+
+function endState(){
+ground.velocityX = 0;
+obstaclesGroup.setVelociiyXEach =0;
+planetGroup.setVelocityXEach=0;
+gameover.visible=true;
 }
 var obstacle;
 function spawnObstacles() {
   if(frameCount % 70 === 0) {
-     obstacle = createSprite(600,400,10,40);
+     obstacle = createSprite(600,400);
     //obstacle.debug = true;
     obstacle.velocityX = -6;
-    
+    obstacle.setCollider("rectangle",0,0,100,160);
+    obstacle.debug= false;
     //generate random obstacles
     var rand = Math.round(random(1,3));
     switch(rand) {
@@ -181,7 +235,7 @@ function spawnObstacles() {
     obstacle.y=Math.round(random(50,440))
     //assign scale and lifetime to the obstacle           
     //obstacle.scale = 0.5;
-    obstacle.lifetime = 300;
+    obstacle.lifetime = 800;
     obstacle.depth = ironman.depth;
     ironman.depth+=1
     //add each obstacle to the group
